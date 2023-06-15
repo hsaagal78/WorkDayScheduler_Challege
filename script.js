4d761923801e6a6bd4264d7f77d9b51b87ae8751
hsagal78 <hsagal78@gmail.com>
2023-06-15 16:18:32 -0400

    fix localStorage

--- script.js
+++ script.js
@@ -33,11 +33,11 @@ $(function() {
     function loadSavedEvents() {
       saveBtns.each(function() {
         var hour = $(this).data("hour"); // Get the value of the "data-hour" attribute of the current element
-        var event = localStorage.getItem(hour);
-        console.log(localStorage.getItem(hour)); // Retrieve the saved event from local storage based on the hour key
-        var inputField = $("#hour-" + hour); // Get the input field element with id "hour-hourValue"
+        var event = localStorage.getItem(hour);// Retrieve the saved event from local storage based on the hour key
+       
+        var inputField = $(this).siblings(".description"); // Get the input field element with id "hour-hourValue"
   
-        if (event !== null) { // Check if the event is not null
+        if (event !== null && event !== undefined) { // Check if the event is not null
           inputField.val(event); // Set the value of the input field to the retrieved event
         }
       });
@@ -45,16 +45,19 @@ $(function() {
   
     // Save the event to local storage when the save button is clicked
     function saveEvent(event) {
-      var row = $(event.target).closest(".row"); // Find the closest parent element with the class "row"
+      var saveBtn=$(this);
+      var row = saveBtn.closest(".row"); // Find the closest parent element with the class "row"
       var textarea = row.find(".description"); // Select the textarea element within the row
-      var hour = $(event.target).data("hour"); // Get the value of the "data-hour" attribute from the element that triggered the event
+      var hour = saveBtn.data("hour"); // Get the value of the "data-hour" attribute from the element that triggered the event
       var value = textarea.val().trim(); // Retrieve the value of the textarea and remove leading/trailing whitespace
   
       if (value !== "") {
         localStorage.setItem(hour, value); // Store the value in local storage with the hour as the key
+        
       } else {
         localStorage.clear(); // If the value is empty, remove all items from local storage
       }
+      
     }
   
     saveBtns.on("click", saveEvent); // Bind the saveEvent function as the click event listener for each save button
@@ -70,35 +73,4 @@ $(function() {
     }, 60000);
   });
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-// });
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
+ 
